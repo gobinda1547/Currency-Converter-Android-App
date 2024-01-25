43ca19315097cc05ba210d76527522a2e34c0367
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-01-26 04:23:47 +0600

    Title - Combined currency name + exchange rate & available country images in data layer Description - Combined currency name + exchange rate & available country images in data layer

--- app/src/main/java/com/gobinda/currency/converter/data/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/gobinda/currency/converter/data/repository/AppRepositoryImpl.kt
@@ -3,9 +3,10 @@ package com.gobinda.currency.converter.data.repository
 import android.content.Context
 import com.gobinda.currency.converter.R
 import com.gobinda.currency.converter.common.AppRepository
+import com.gobinda.currency.converter.data.model.CurrencyInfo
 import com.gobinda.currency.converter.data.source.OpenExchangeApi
+import com.gobinda.currency.converter.imagekit.CountryImageProvider
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,11 +18,8 @@ class AppRepositoryImpl @Inject constructor(
     private val openExchangeApi: OpenExchangeApi
 ) : AppRepository {
 
-    private val _currencies = MutableStateFlow<Map<String, String>?>(null)
-    override val currencies: StateFlow<Map<String, String>?> = _currencies
-
-    private val _exchangeRate = MutableStateFlow<Map<String, Double>?>(null)
-    override val exchangeRate: StateFlow<Map<String, Double>?> = _exchangeRate
+    private val _currencyInfo = MutableStateFlow<Map<String, CurrencyInfo>?>(null)
+    override val currencyInfo: StateFlow<Map<String, CurrencyInfo>?> = _currencyInfo
 
     private val _requestStatus = MutableStateFlow<RequestStatus>(RequestStatus.NotStarted)
     override val requestStatus: StateFlow<RequestStatus> = _requestStatus.asStateFlow()
@@ -30,44 +28,46 @@ class AppRepositoryImpl @Inject constructor(
         withContext(Dispatchers.IO) {
             _requestStatus.tryEmit(RequestStatus.Started)
 
-            var successfulResponseCounter = 0
             val appIdForServer = context.getString(R.string.APP_ID_OPEN_EXCHANGE_SERVER)
             val queryMap = mapOf("app_id" to appIdForServer)
 
-            val currencyDeferred = async {
-                try {
-                    val currencyResponse = openExchangeApi.getCurrencies(queryMap)
-                    if (currencyResponse.isSuccessful) {
-                        currencyResponse.body()?.let { currencyValue ->
-                            _currencies.tryEmit(currencyValue)
-                            successfulResponseCounter++
-                        }
-                    }
-                } catch (ignore: Exception) {
+            val currencyDef = asyncSafeCall { openExchangeApi.getCurrencies(queryMap) }
+            val exchangeRateDef = asyncSafeCall { openExchangeApi.getExchangeRate(queryMap) }
+
+            val currencyResponse = currencyDef.await()
+            val exchangeRateResponse = exchangeRateDef.await()
+
+            val currencyData = currencyResponse?.let {
+                when (it.isSuccessful) {
+                    true -> it.body()
+                    else -> null
                 }
             }
-            val exchangeRateDeferred = async {
-                try {
-                    val exchangeRateResponse = openExchangeApi.getExchangeRate(queryMap)
-                    if (exchangeRateResponse.isSuccessful) {
-                        exchangeRateResponse.body()?.let { exchangeRateValue ->
-                            _exchangeRate.tryEmit(exchangeRateValue.rates)
-                            successfulResponseCounter++
-                        }
-                    }
-                } catch (ignore: Exception) {
+            val exchangeRateData = exchangeRateResponse?.let {
+                when (it.isSuccessful) {
+                    true -> it.body()?.rates
+                    else -> null
                 }
             }
 
-            currencyDeferred.await()
-            exchangeRateDeferred.await()
+            if (currencyData == null || exchangeRateData == null) {
+                _requestStatus.emit(RequestStatus.Failed)
+                return@withContext
+            }
 
-            _requestStatus.emit(
-                when (successfulResponseCounter == 2) {
-                    true -> RequestStatus.Successful
-                    else -> RequestStatus.Failed
-                }
-            )
+            val finalResult = currencyData.entries.mapNotNull { mapEntry ->
+                val rateValue = exchangeRateData[mapEntry.key] ?: return@mapNotNull null
+                CurrencyInfo(
+                    codeName = mapEntry.key,
+                    fullName = mapEntry.value,
+                    rate = rateValue,
+                    imageName = CountryImageProvider.getFileNameFromCountryCode(mapEntry.key),
+                    isImageAvailable = CountryImageProvider.doesExist(context, mapEntry.key)
+                )
+            }.associateBy { it.codeName }
+
+            _currencyInfo.emit(finalResult)
+            _requestStatus.emit(RequestStatus.Successful)
         }
     }
 }
